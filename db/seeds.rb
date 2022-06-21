3f15d2b366e4f57bc74b67bcebd1d696a939a335
digital-diver <digital-diver@protonmail.com>
2022-06-21 18:27:06 +0200

    add seeds.rb & controller actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+2.times do |i|
+  User.create!( username: "Joshua #{i+1}", password: 'password' )
+end
+# . . .
+2.times do |i|
+  Book.create!( book: "Testing Book #{i+1}", likes_books: i+1, user_id: 1 )
+end
+# . . .
+2.times do |i|
+  Book.create!( book: "Testing Book #{i+3}", likes_books: i+3, user_id: 2 )
+end
+# . . .
+2.times do |i|
+  Subject.create!( subject: "I am subject #{i+1}", likes_subjects: i+1, book_id: 1 )
+end
+2.times do |i|
+  Subject.create!( subject: "I am subject #{i+3}", likes_subjects: i+3, book_id: 2 )
+end
